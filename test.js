f07c6a41a31da941488c35b134cbb76043b3638b
mathericc <49539020+mathericc@users.noreply.github.com>
2019-04-16 19:59:45 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
+var x = 200;
+var y = 200;
 void setup ()
 {
   size(1280,720);
   background(100, 100, 100);
 }
 
-//var
-var x = 200;
-var y = 200;
 
 void draw ()
 {
   fill(0);
   ellipse(x,y,200,200);
-  x ++;
-  if (x> 1280)
+  x += 1;
+  if (x > 1280)
   {
     x = 0;
   }
