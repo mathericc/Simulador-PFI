6a1d9b497b96294a4911aa3ff0a021e4c76991e9
mathericc <49539020+mathericc@users.noreply.github.com>
2019-07-02 12:19:53 -0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,7 +1,16 @@
-var Objects = function(x,y,w,h)
+var Unphysical = function(x,y,w,h)
 {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
+}
+
+var Physical = function(x, y, w, h)
+{
+  Unphysical.call(this,x,y,w,h);
+  this.positon = new PVector(0,0);
+  this.velocity = new PVector(0,0);
+  this.acceleration = new PVector(0,0);
+
 }
