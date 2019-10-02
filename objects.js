1e850b607e98248edc9fa82d08d089a63adf75ab
mathericc <mathericc@gmail.com>
2019-10-02 16:17:06 -0300

    atrito 2

--- objects.js
+++ objects.js
@@ -21,7 +21,7 @@ var Physical = function(x=200, y=200, w=10, h=10, mass=1, density=1)
   this.friction = 0;
 }
 
-Physical.prototype.define_friction()
+Physical.prototype.define_friction = function()
 {
     if ( this.velocity.mag() != 0 ) //Dinâmico
     {
@@ -44,6 +44,7 @@ Physical.prototype.update = function()
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
     this.acceleration.mult(0);
+    this.define_friction();
 }
 
 
